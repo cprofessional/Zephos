17da87d1e70007380f5f028347307fcb90fc415b
CJ <CJMustard21@gmail.com>
2025-02-09 20:23:12 -0600

    favicon

--- app/page.tsx
+++ app/page.tsx
@@ -14,28 +14,31 @@ export default function Home() {
   >([]);
 
   const handleSend = () => {
-    handleSendButton(inputValue, setInputValue, setStartMode, setPastMessages, ai, pastMessages);
+    handleSendButton(
+      inputValue,
+      setInputValue,
+      setStartMode,
+      setPastMessages,
+      ai,
+      pastMessages
+    );
   };
 
   return (
-    <div
-    className={`min-h-screen bg-[#1E1F1E] text-white flex items-center justify-center`}
-  >
+    <div className="bg-[#1E1F1E] text-white flex items-center justify-center min-h-screen">
+
     {startMode && <Messages pastMessages={pastMessages} />}
     <div
-      className={`w-full max-w-3xl space-y-6 ${
-        !startMode ? `` : `absolute bottom-10`
-      }`}
-    >
+      className={`w-full max-w-3xl space-y-6 ${ !startMode ? `` : `absolute bottom-10` }`} >
       {!startMode && (
         <h1 className="text-4xl font-semibold text-center">
           What can I help with?
         </h1>
       )}
-      <SearchBar 
-        inputValue={inputValue} 
-        onInputChange={(e) => handleInputChange(e, setInputValue)} 
-        onSend={handleSend} 
+      <SearchBar
+        inputValue={inputValue}
+        onInputChange={(e) => handleInputChange(e, setInputValue)}
+        onSend={handleSend}
       />
     </div>
   </div>
